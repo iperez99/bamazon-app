2c88b5a6e2fca0ce3d97eec0415f4592415cf718
Qbisst <ismaelaperez@outlook.com>
2019-04-23 20:31:26 -0400

    finished seeds.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,16 @@
 CREATE DATABASE bamazon;
 USE bamazon;
 
-CREATE TABLE products( 
-    item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR(255) NOT NULL,
-    department_name VARCH(255) NOT NULL,
-    price INTEGER (11) NOT NULL,
-    stock_quantity INTEGER (11)  
+CREATE TABLE products
+(
+    item_id INTEGER(11)
+    AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR
+    (255) NOT NULL,
+    department_name VARCH
+    (255) NOT NULL,
+    price INTEGER
+    (11) NOT NULL,
+    stock_quantity INTEGER
+    (11)  
 )
---
+++ seeds.sql
@@ -0,0 +1,39 @@
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Xbox ONE S", "Video Games", 299, 15);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Halo Infinite", "Video Games", 59, 20);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Wireless Controller", "Video Games", 49, 35);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("AAA batteries", "Electronics", 5, 100);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("USB-C charger", "Electronics", 20, 45);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Surface Laptop 2", "Electronics", 1100, 10);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Body Soap", "Bath & Essentials", 3, 25);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Tooth Paste", "Bath & Essentials", 4, 60);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Mechanical Pencil", "Office", 2, 115);
+
+INSERT INTO products
+    (product_name, department_name, price, stock_quantity)
+VALUES("Sticky Notes", "Office", 3, 200);
