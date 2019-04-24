2c6e61f9f66f6a1ef3eba56fa94fe891381e46fa
Qbisst <ismaelaperez@outlook.com>
2019-04-23 20:11:22 -0400

    finished schema.sql

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products( 
+    item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    department_name VARCH(255) NOT NULL,
+    price INTEGER (11) NOT NULL,
+    stock_quantity INTEGER (11)  
+)
