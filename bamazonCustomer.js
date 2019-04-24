0226a2d141db4e7135cbb8aa353e256d51863965
Qbisst <ismaelaperez@outlook.com>
2019-04-23 21:38:30 -0400

    updated mysql connection info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,3 +4,17 @@ var inquirer = require("inquirer");
 var chalk = require("chalk");
 var Table = require("cli-table");
 
+// Variables that will be used in the inquirer function
+var productId;
+var productName;
+
+//MySQL connection information
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
